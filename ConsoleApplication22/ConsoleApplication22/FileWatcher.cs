a9ffbe3c64a249534e056d3cfde72b3bc5fed864
DimaNikonov <vipergordon@outlook.com>
2017-05-31 20:07:24 +0300

    Add files via upload
    uio

---
+++ ConsoleApplication22/ConsoleApplication22/FileWatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+
+    class FileWatcher
+    {
+      
+        DateTime timeLast;
+        string file;
+        public event Action<DateTime> FileChanged; 
+        public FileWatcher(string file)
+        {
+            this.file = file;
+        }
+        
+       public  void FileChange ()
+        {
+            while(true)
+            {
+                if(timeLast!=File.GetLastWriteTime(file))
+                {
+                    timeLast = File.GetLastWriteTime(file);
+                    FileChanged?.Invoke(timeLast);
+                    Console.WriteLine("File Changed");
+                }
+            }
+        }
+         
+    }
+}
---
+++ ConsoleApplication22/ConsoleApplication22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileWatcher fw = new FileWatcher("ControlTask.xt");
+            fw.FileChange();
+        }
+    }
+}
